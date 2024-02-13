eddbcc6b7a64d3048bd1c37e0850239047d9a9c5
TEO-INTL\Muhammad.Shahab <muhammad.shahab@teo-intl.com>
2024-02-13 15:39:14 +0500

    add new resouce and sql

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './auth/auth.module';
 import configuration from 'config/configuration';
 
 
@@ -11,6 +12,7 @@ import configuration from 'config/configuration';
       isGlobal: true,
       load: [configuration]
     }),
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
